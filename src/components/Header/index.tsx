faf89c354118aa3654410137130ec6e6b295e85c
Marcosgtdev <Marcosgtdev>
2025-01-06 21:12:39 -0300

    FEAT: adicionando logo e buttons no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image';
+
 const Header = () => {
     return (
         <header className="bg-white shadow">
-            <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+            <div className="container mx-auto px-4 py-4 flex justify-between items-center bg-red-300">
                 <div className="flex items-center">
+                    <div>
+                        <Image src="/assets/arymugs_logo.svg" alt="logo" width={150} height={150} />
+                    </div>
                     <div className="ml-4">
                         <input
                             type="text"
@@ -11,12 +16,20 @@ const Header = () => {
                         />
                     </div>
                     <nav className="ml-4">
-                        <a href="#" className="px-4 py-2 text-gray-700">Live Code</a>
-                        <a href="#" className="px-4 py-2 text-gray-700">Bibliotech</a>
+                        <a href="#" className="px-4 py-2 text-gray-700">Produtos</a>
+                        <a href="#" className="px-4 py-2 text-gray-700">Favoritos</a>
+                        <a href="#" className="px-4 py-2 text-gray-700">Carrinho</a>
                     </nav>
                 </div>
                 <div className="flex items-center">
-                    <a href="#" className="px-4 py-2 text-gray-700">Home</a>
+                    <button
+                        className="bg-transparent hover:bg-pink-600 text-white font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded">
+                        Entrar
+                    </button>
+                    <button
+                        className="bg-pink-600 hover:bg-pink-700 text-white font-bold py-2 px-4 border border-white rounded">
+                        Criar conta
+                    </button>
                 </div>
             </div>
         </header>
