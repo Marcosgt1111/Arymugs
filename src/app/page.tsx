f323cf32953bcc3a57b405305b315ed62f65df64
Marcosgtdev <Marcosgtdev>
2025-02-13 22:32:49 -0300

    FEAT: Main adicionado ao projeto com BG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Main from '../components/main';
 
 const Page: React.FC = () => {
     return (
         <>
             <Header />
-            <main>
-                <h1>Welcome to My Page</h1>
-                <p>This is a sample page.</p>
-            </main>
+            <Main />
         </>
     );
 };
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,14 @@
+const Main = () => {
+    return (
+        <div
+            className="h-screen w-screen flex justify-center items-center relative"
+            style={{
+                backgroundImage: 'url("images/bg.jpg")',
+            }}
+        >
+        </div>
+    );
+};
+
+export default Main;
+
